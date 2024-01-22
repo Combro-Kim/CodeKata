4f4ef4dbb435762f017e31ef107267b66c38a2a4
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-01-22 15:24:37 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 60.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var sum: Int = 0
+        for (i in 0..n)
+            if (i % 2 == 0)
+                sum += i
+        return sum
+    }
+}
