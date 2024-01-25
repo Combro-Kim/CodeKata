f2bfbaa12104621c220a03c98eb78090e3fcc4ca
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-01-25 15:50:06 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.13 ms, Memory: 58.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun solution(a: Int, b: Int): Long {
+        var sum: Long = 0
+        var a_num = a
+        var b_num = b
+        if (b<a) {
+            a_num = b
+            b_num = a
+        }else if (a==b){
+            return a.toLong()
+        }
+        for (i in a_num..b_num) {
+            sum += i
+        }
+        return sum.toLong()
+    }
+}
