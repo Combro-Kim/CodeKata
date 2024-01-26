469ab9bb46de36721102acdc1bd753cd17d9fa5c
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-01-26 14:30:05 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 15.50 ms, Memory: 59.1 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
@@ -4,12 +4,14 @@ class Solution {
         val nSize = phone_number.length - 4
         var cnt = 0
 
-        for (i in 0 until nSize) {
-            answer += '*'
+        for (i in 0 until phone_number.length) {
+            if (i < nSize) {
+                answer += '*'
+            } else {
+                answer += phone_number[i]
+            }
         }
-        for (i in nSize until nSize + 4) {
-            answer += phone_number[i]
-        }
+
         return answer
     }
 }
