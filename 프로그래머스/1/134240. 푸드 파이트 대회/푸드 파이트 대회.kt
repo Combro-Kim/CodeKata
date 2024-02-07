798a386f84b0dae6bf2d78f8a8359061e279f751
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-02-07 14:41:18 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 27.66 ms, Memory: 61.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(food: IntArray): String {
+        var answer: String = ""
+        for (i in 1 until food.size) {
+            for (v in 1..food[i] / 2) {
+                answer += i
+            }
+        }
+            return answer + 0 + answer.reversed()
+    }
+}
