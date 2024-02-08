7ce46e734eff74de101ef41c9b88f7ed10af8e48
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-02-08 14:43:39 +0900

    [level 1] Title: 과일 장수, Time: 22.43 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun solution(k: Int, m: Int, score: IntArray): Int {
+        var answer: Int = 0
+        score.sortDescending()
+
+        for (i in 0 until score.size / m) {
+            var arr = intArrayOf()
+            var cnt = m*i
+            for (j in 0 until m) {
+                arr += score[j+cnt]
+            }
+            answer += arr[m-1] * m
+        }
+
+        return answer
+    }
+}
