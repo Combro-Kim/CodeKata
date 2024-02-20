2259ba3a22f3e9e03cfaee124028858d2d7935a2
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-02-20 11:27:08 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.28 ms, Memory: 59.6 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.kt
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.kt
@@ -1,10 +1,11 @@
 class Solution {
     fun solution(X: String, Y: String): String {
         var answer = StringBuilder()
-        val arrX = IntArray(10)
-        val arrY = IntArray(10)
-        countNumInArr(X, arrX)
-        countNumInArr(Y, arrY)
+        var arrX = IntArray(10)
+        var arrY = IntArray(10)
+        countArr(X, arrX)
+        countArr(Y, arrY)
+
         for (i in arrX.indices.reversed()) {
             while (arrX[i] > 0 && arrY[i] > 0) {
                 arrX[i]--
@@ -13,20 +14,21 @@ class Solution {
                 answer.append(i)
             }
         }
-
         if (answer.toString() == "") {
             return "-1"
-        } else if (answer.toString().startsWith("0")) {
+        } else if (answer.startsWith("0")) {
             return "0"
         } else {
             return answer.toString()
         }
+
+
     }
 }
 
-private fun countNumInArr(str: String, arr: IntArray) {
+private fun countArr(str: String, arr: IntArray) {
     for (i in str) {
-        val index = Integer.parseInt(i.toString()) - 0
+        val index = i.toString().toInt()
         arr[index]++
     }
 }
