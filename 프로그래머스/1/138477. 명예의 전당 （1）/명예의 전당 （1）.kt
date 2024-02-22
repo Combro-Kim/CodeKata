a62297778886a0ccf68f53cfb0ef0088e713a329
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-02-22 11:47:26 +0900

    [level 1] Title: 명예의 전당 (1), Time: 13.56 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.kt
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.kt
@@ -1,17 +1,16 @@
 class Solution {
     fun solution(k: Int, score: IntArray): IntArray {
         var answer: IntArray = intArrayOf()
-        var arrK = mutableListOf<Int>() //k 순위
+        var tear = mutableListOf<Int>()
 
         for (i in score) {
-
-            if (arrK.size < k) { //k만큼 arr에 집어넣기
-                arrK.add(i)
-            } else if (arrK[0] < i) {
-                arrK[0] = i
+            if (tear.size < k) {
+                tear.add(i)
+            } else if (tear[0] < i) {
+                tear[0] = i
             }
-            arrK.sort()
-            answer += arrK[0]
+            tear.sort()
+            answer += tear[0]
         }
         return answer
     }
