d293f5629b6724d6ce897bea68eb8d673db64175
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-03-01 21:28:24 +0900

    [level 1] Title: 모의고사, Time: 15.66 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.kt
+++ 프로그래머스/1/42840. 모의고사/모의고사.kt
@@ -1,9 +1,9 @@
 class Solution {
     fun solution(answers: IntArray): IntArray {
         var answer = intArrayOf()
-        var arr1 = intArrayOf(1, 2, 3, 4, 5)
-        var arr2 = intArrayOf(2, 1, 2, 3, 2, 4, 2, 5)
-        var arr3 = intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
+        val arr1 = intArrayOf(1, 2, 3, 4, 5)
+        val arr2 = intArrayOf(2, 1, 2, 3, 2, 4, 2, 5)
+        val arr3 = intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
 
         var cnt = arrayOf(0, 0, 0)
 
@@ -21,7 +21,7 @@ class Solution {
         val cntMax = cnt.maxOrNull()
         for (i in cnt.indices) {
             if (cnt[i] == cntMax) {
-                answer += i+1
+                answer += i + 1
             }
         }
         return answer
