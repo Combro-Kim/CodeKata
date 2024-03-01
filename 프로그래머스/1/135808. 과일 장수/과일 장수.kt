f4f413e94d69abef27a98b4eea9171ae329ec811
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-03-01 21:12:55 +0900

    [level 1] Title: 과일 장수, Time: 22.27 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.kt
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.kt
@@ -5,9 +5,9 @@ class Solution {
 
         for (i in 0 until score.size / m) {
             var arr = intArrayOf()
-            var cnt = m*i
+            var cnt = m * i
             for (j in 0 until m) {
-                arr += score[j+cnt]
+                arr += score[j + cnt]
             }
             answer += arr[m-1] * m
         }
