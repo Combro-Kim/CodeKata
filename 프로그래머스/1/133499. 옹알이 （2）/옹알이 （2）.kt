66b9404b3bcfe9c092936f91148bbfbbc13a7ce2
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-03-06 09:13:48 +0900

    [level 1] Title: 옹알이 (2), Time: 12.41 ms, Memory: 59.6 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.kt
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.kt
@@ -1,11 +1,11 @@
 class Solution {
     fun solution(babbling: Array<String>): Int {
         var answer: Int = 0
-        var regex = "aya|ye|woo|ma".toRegex()
-        var regexC = "ayaaya|yeye|woowoo|mama".toRegex()
+        val regex = "aya|ye|woo|ma".toRegex()
+        val regexX = "ayaaya|yeye|woowoo|mama".toRegex()
         
-        answer = babbling.map { it.replace(regexC,"*") }.map { it.replace(regex,"") }.count { it=="" }
-        
+        answer = babbling.map { it.replace(regexX,"*") }.map { it.replace(regex,"") }.count{it==""}
+
         return answer
     }
 }
