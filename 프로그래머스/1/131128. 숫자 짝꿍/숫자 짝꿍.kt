62db33f044779330f169ac46688b52fceeab7b20
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-03-07 09:34:07 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.10 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.kt
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.kt
@@ -1,28 +1,28 @@
 class Solution {
     fun solution(X: String, Y: String): String {
         var answer = StringBuilder()
-        val arrX = IntArray(10)
-        val arrY = IntArray(10)
+        var arrX = IntArray(10)
+        var arrY = IntArray(10)
         countArr(X, arrX)
         countArr(Y, arrY)
 
-        for (i in arrX.indices.reversed()) {
-            while (arrX[i] > 0 && arrY[i] > 0) {
+        for(i in arrX.indices.reversed()){
+            while(arrX[i]>0 && arrY[i]>0){
                 arrX[i]--
                 arrY[i]--
                 answer.append(i)
             }
         }
-        if(answer.toString() ==""){
+
+        if (answer.isEmpty()) {
             return "-1"
-        }else if (answer.startsWith("0")){
+        } else if (answer.startsWith("0")) {
             return "0"
-        }else{
-            return answer.toString()
         }
+        return answer.toString()
     }
 
-    private fun countArr(str: String, arr: IntArray) {
+    fun countArr(str: String, arr: IntArray) {
         for (i in str) {
             arr[i.toString().toInt()]++
         }
