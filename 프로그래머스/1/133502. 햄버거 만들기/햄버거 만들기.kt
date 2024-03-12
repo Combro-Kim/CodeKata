bd00ca553c93caea87276adf2631843ed4aa0797
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-03-12 09:54:59 +0900

    [level 1] Title: 햄버거 만들기, Time: 26.35 ms, Memory: 64.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(ingredient: IntArray): Int {
+        var answer: Int = 0
+        val arr = listOf(1, 2, 3, 1)
+        var muList = mutableListOf<Int>()
+
+        for(i in ingredient){
+            muList.add(i)
+            if( muList.size >=4 && muList.slice(muList.size-4 until muList.size)==arr){
+                repeat(4) { muList.removeLast()}
+                answer++
+            }
+        }
+        return answer
+    }
+}
