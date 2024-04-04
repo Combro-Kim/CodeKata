20255d0b689c115789b072dcb348084e9a428a7c
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-04-04 09:56:29 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.kt
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.kt
@@ -2,10 +2,10 @@ class Solution {
     fun solution(n: Int, a: Int, b: Int): Int {
         var answer = 0
         var a = a ;var b = b
-        
+
         while (a != b) {
-            a = (a + 1) / 2
-            b = (b + 1) / 2
+            a -= a/2
+            b -= b/2
             answer++
         }
         return answer
