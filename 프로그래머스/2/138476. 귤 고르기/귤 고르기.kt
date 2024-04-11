1b680ec66612949aed3eeac58eaf188944db1819
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-04-11 20:16:15 +0900

    [level 2] Title: 귤 고르기, Time: 74.66 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.kt
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.kt
@@ -1,12 +1,10 @@
 class Solution {
     fun solution(k: Int, tangerine: IntArray): Int {
         var answer = 0
-
-        val list = tangerine.groupBy { it }.values.map { it.size }
-        
-        val sortList = list.sortedDescending()
+        val list = tangerine.groupBy { it }.values.map { it.size }.toList().sortedDescending()
+//        val sortList = list.sortedDescending()
         var cnt = 0
-        sortList.takeWhile {
+        list.takeWhile {
             cnt += it
             answer++
             cnt < k
