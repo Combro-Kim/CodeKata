58685a2813a2040175400a71985765025415dd3e
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-04-12 19:47:03 +0900

    [level 2] Title: 괄호 회전하기, Time: 10.22 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.kt
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.kt
@@ -1,32 +1,23 @@
 class Solution {
-    fun isCorrect(s: String): Boolean {
-        val stack = mutableListOf<Char>()
-        val brackets = mapOf(')' to '(', ']' to '[', '}' to '{')
+    fun solution(s: String): Int {
+        var count = 0
+        for (i in 0 until s.length) {
+            val rotatedString = s.substring(i) + s.substring(0, i)
+            if (isValid(rotatedString)) count++
+        }
+        return count
+    }
 
+    private fun isValid(s: String): Boolean {
+        val stack = mutableListOf<Char>()
         for (char in s) {
-            if (brackets.containsKey(char)) {
-                if (stack.isEmpty() || stack.last() != brackets[char]) {
-                    return false
-                }
-                stack.removeAt(stack.size - 1)
-            } else {
-                stack.add(char)
+            when (char) {
+                '(', '[', '{' -> stack.add(char)
+                ')' -> if (stack.isEmpty() || stack.removeAt(stack.lastIndex) != '(') return false
+                ']' -> if (stack.isEmpty() || stack.removeAt(stack.lastIndex) != '[') return false
+                '}' -> if (stack.isEmpty() || stack.removeAt(stack.lastIndex) != '{') return false
             }
         }
         return stack.isEmpty()
-    }
-
-    fun solution(s: String): Int {
-        var answer = 0
-        val rotatedS = s.toMutableList()
-
-        for (i in 0 until s.length) {
-            rotatedS.add(rotatedS.removeAt(0))
-            if (isCorrect(rotatedS.joinToString(""))) {
-                answer++
-            }
-        }
-
-        return answer
     }
 }
