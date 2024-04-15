f1072d555391c4f8f108a6b488daabe3f99e16b9
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-04-15 09:53:00 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 364.47 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.kt
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.kt
@@ -1,12 +1,12 @@
 class Solution {
     fun solution(elements: IntArray): Int {
         val answer = mutableSetOf<Int>()
-        val elements2 = elements + elements
+        val elements2 = elements + elements //[7, 9, 1, 1, 4, 7, 9, 1, 1, 4]
 
-        for(i in 1 .. elements.size){
-            for(j in elements.indices){
+        for (i in 1..elements.size) {
+            for (j in elements.indices) {
                 var sum = 0
-                for(k in j until i+j){
+                for(k in j until j + i){
                     sum += elements2[k]
                 }
                 answer.add(sum)
