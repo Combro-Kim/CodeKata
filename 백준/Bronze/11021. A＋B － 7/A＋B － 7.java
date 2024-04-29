c8ceebf9e4027368161aeddf43261827af74215b
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-04-29 09:06:01 +0900

    [Bronze V] Title: A+B - 7, Time: 272 ms, Memory: 18288 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 1; i <= t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.printf("Case #%d: %d\n", i, a + b);
+        }
+    }
+}
