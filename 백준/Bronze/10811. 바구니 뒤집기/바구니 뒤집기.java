5aeb78381271f5ca2b66c026f81b4e3858889837
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-05-03 09:27:40 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 236 ms, Memory: 17896 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int arr[] = new int[n];
+        for (int idx = 0; idx < n; idx++) {
+            arr[idx] = idx + 1;
+        }
+
+
+        for (int tc = 0; tc < m; tc++) {
+            int i = sc.nextInt() - 1;
+            int j = sc.nextInt() - 1;
+            
+            for (int c = i; c <= j; c++) {
+                int jj = j--;
+                int tmp = arr[c];
+                arr[c] = arr[jj];
+                arr[jj] = tmp;
+            }
+        }
+        String result = Arrays.toString(arr).replaceAll("[\\[\\],]", "");
+        System.out.printf(result);
+    }
+}
