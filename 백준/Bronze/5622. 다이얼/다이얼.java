f6a17cac829d6c9ab1cc1871dcb01793a3dfda8b
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-05-08 09:28:55 +0900

    [Bronze II] Title: 다이얼, Time: 208 ms, Memory: 17456 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int cnt = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+//            switch (s.charAt(i)){
+//
+//                case 'A': case 'B': case 'C':
+//                    cnt += 3;
+//            }
+            char c = s.charAt(i);
+            if (c >= 'A' && c <= 'C') {
+                cnt += 3;
+            } else if (c >= 'D' && c <= 'F') {
+                cnt += 4;
+            } else if (c >= 'G' && c <= 'I') {
+                cnt += 5;
+            } else if (c >= 'J' && c <= 'L') {
+                cnt += 6;
+            } else if (c >= 'M' && c <= 'O') {
+                cnt += 7;
+            } else if (c >= 'P' && c <= 'S') {
+                cnt += 8;
+            } else if (c >= 'T' && c <= 'V') {
+                cnt += 9;
+            } else if (c >= 'W' && c <= 'Z') {
+                cnt += 10;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
