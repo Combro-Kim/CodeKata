e00363dbb2d1b28d28f3dd71495e947c2bf5f292
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-05-24 09:50:56 +0900

    [level 0] Title: 문자열안에 문자열, Time: 8.51 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.kt
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.kt
@@ -1,9 +1,3 @@
 class Solution {
-    fun solution(str1: String, str2: String): Int {
-        var result = 2
-        if(str1.contains(str2)){
-            result =1
-        }
-        return result
-    }
+    fun solution(str1: String, str2: String) = if(str1.contains(str2)) 1 else 2
 }
