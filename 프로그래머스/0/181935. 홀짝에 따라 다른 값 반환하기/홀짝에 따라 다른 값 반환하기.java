14a2e7796fec5229ea5386e32f082376b746ad47
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-08-07 17:53:41 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int even = 0;
+        int odd = 0;
+
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 == 0) {
+                even += i*i;
+            } else {
+                odd += i;
+            }
+        }
+        return n % 2 == 0 ? even : odd;
+    }
+}
