1d521ade36bbec641f48b5a57da74c118a526ae6
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2024-09-13 19:55:35 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 15.03 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,11 +1,11 @@
 class Solution {
-    public String solution(int[] food) {
-        StringBuilder sb = new StringBuilder();
+	public String solution(int[] food) {
+		StringBuilder sb = new StringBuilder();
 
-        for (int i = 1; i < food.length; i++) {
-            sb.append(String.valueOf(i).repeat(food[i] / 2));
-        }
+		for (int i = 1; i < food.length; i++) {
+			sb.append(String.valueOf(i).repeat(food[i]/2));
+		}
 
-        return sb.toString() + "0" + sb.reverse();
-    }
+		return sb.toString()+'0'+sb.reverse();
+	}
 }
