0ad9507304bfcc7f5ec468d2df2791f1000e1356
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2025-08-25 14:58:37 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,20 +1,17 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] arr = new int[num_list.length+1];
-
-        for(int i =0; i<num_list.length;i++){
-            arr[i] = num_list[i];
+        int[] answer = new int[num_list.length+1];
+        
+        for(int i =0; i<num_list.length; i++) {
+        	answer[i] = num_list[i];
         }
-
-        int last = num_list[num_list.length-1];
-        int secondLast = num_list[num_list.length-2];
-
-        if(last > secondLast){
-            arr[num_list.length] = last - secondLast;
-        }else{
-            arr[num_list.length] = last * 2;
+        
+        if(num_list[num_list.length -2] < num_list[num_list.length-1]) {
+        	answer[answer.length -1] = num_list[num_list.length-1] - num_list[num_list.length-2];
+        }else {
+        	answer[answer.length-1] = num_list[num_list.length-1] * 2;
         }
-
-        return arr;
+        
+        return answer;
     }
 }
